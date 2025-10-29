1cdd5c414a9e6ed5f4200a4dd5200013d1977071
Leonardo Campos <leonardocampos@MacBook-Air-de-Leonardo.local>
2025-10-29 18:29:32 -0300

    retoques v1

--- src/conexion/DBConnection.java
+++ src/conexion/DBConnection.java
@@ -12,12 +12,12 @@ public class DBConnection {
 
         // 1. Parámetros de conexión fijos (Hardcoded)
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver"; // y que es com.mysql.cj.jdbc.Driver url moderno, el del evelia esta ambiguo
-    private static final String URL = "escribir tu url";
+    private static final String URL = "jdbc:mysql://localhost:3306/proyectoBD?allowPublicKeyRetrieval=true&useSSL=false";
     //url celina: "jdbc:mysql://localhost:3306/proyectoBD"
     //url leo: "jdbc:mysql://localhost:3306/proyectoBD?allowPublicKeyRetrieval=true&useSSL=false"
-    private static final String USER = "tu_user"; // agregar tu user
+    private static final String USER = "root"; // agregar tu user
     //user celina y leo : "root"
-    private static final String PASSWORD = "tu_password"; // agregar tu clave
+    private static final String PASSWORD = "password"; // agregar tu clave
     //clave celina : "celina"
     //clave leo : "password"
     
